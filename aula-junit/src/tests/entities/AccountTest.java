fa115332b5872077962d80d7319ff6810e006845
Lucas Pedroso <lucaseduardosilvapedroso@gmail.com>
2022-09-29 11:48:04 -0300

    Add withdraw tests

--- aula-junit/src/tests/entities/AccountTest.java
+++ aula-junit/src/tests/entities/AccountTest.java
@@ -11,8 +11,8 @@ public class AccountTest {
     @Test
     public void depositShouldIncreaseBalanceWhenPositiveAmount() {
         // Arrange
-        Double amount = 200.0;
-        Double expectedBalance = 196.0;
+        double amount = 200.0;
+        double expectedBalance = 196.0;
         Account account = AccountFactory.createEmptyAccount();
         // Act
         account.deposit(amount);
@@ -23,12 +23,50 @@ public class AccountTest {
     @Test
     public void depositShouldDoNothingWhenNegativeAmount() {
         // Arrange
-        Double amount = -200.0;
-        Double expectedValue = 100.0;
+        double amount = -200.0;
+        double expectedValue = 100.0;
         Account account = AccountFactory.createAccount(expectedValue);
         // Act
         account.deposit(amount);
         // Assert
         Assertions.assertEquals(expectedValue, account.getBalance());
+    }
+
+    @Test
+    public void fullWithdrawShouldClearBalanceAndReturnFullBalance(){
+        // Arrange
+        double expectedValue = 0.0;
+        double initialBalance = 800.0;
+        Account account = AccountFactory.createAccount(initialBalance);
+        // Act
+        double result = account.fullWithdraw();
+        // Assert
+        Assertions.assertTrue(expectedValue == account.getBalance());
+        Assertions.assertTrue(result == initialBalance);
+    }
+
+    @Test
+    public void withdrawShouldDecreaseBalanceWhenSufficientBalance(){
+        // Arrange
+        double amount = 500.0;
+        double expectedValue = 300.0;
+        Account account = AccountFactory.createAccount(800.0);
+        // Act
+        account.withdraw(amount);
+        // Assert
+        Assertions.assertEquals(expectedValue, account.getBalance());
+    }
+
+    @Test
+    public void withdrawShouldThrowExceptionWhenInsufficientBalance(){
+        // Arrange
+        double amount = 801.0;
+        Account account = AccountFactory.createAccount(800.0);
+        // Act
+        // account.withdraw(amount);
+        // Assert
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            account.withdraw(amount);
+        });
     }
 }
